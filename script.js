7eb842a1b7220e3e15e2d82aed6b11c384bbd223
Alexey Mikhailov <krummi1414@yandex.ru>
2023-05-01 00:34:33 +0300

    feat: change array for row on object for row, add event for key active

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 
-// const keysArr = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+// const keysArr = [];
 
-const keysArr = [];
+const RowKeys1 = { Backquote: '`', Digit1: '1', Digit2: '2', Digit3: '3', Digit4: '4', Digit5: '5', Digit6: '6', Digit7: '7', Digit8: '8', Digit9: '9', Digit0: '0', Minus: '-', Equal: '=', Backspace: 'Backspace' };
+const RowKeys2 = { Tab: 'Tab', KeyQ: 'q', KeyW: 'w', KeyE: 'e', KeyR: 'r', KeyT: 't', KeyY: 'y', KeyU: 'u', KeyI: 'i', KeyO: 'o', KeyP: 'p', BracketLeft: '[', BracketRight: ']', Backslash: '\\', Delete: 'Del' };
+const RowKeys3 = { CapsLock: 'CapsLock', KeyA: 'a', KeyS: 's', KeyD: 'd', KeyF: 'f', KeyG: 'g', KeyH: 'h', KeyJ: 'j', KeyK: 'k', KeyL: 'l', Semicolon: ';', Quote: "'", Enter: 'Enter' };
+const RowKeys4 = { ShiftLeft: 'Shift', KeyZ: 'z', KeyX: 'x', KeyC: 'c', KeyV: 'v', KeyB: 'b', KeyN: 'n', KeyM: 'm', Comma: ',', Period: '.', Slash: '/', ArrowUp: '▲ ', ShiftRight: 'Shift' };
+const RowKeys5 = { ControlLeft: 'Ctrl', MetaLeft: 'Win', AltLeft: 'Alt', Space: '', AltRight: 'Alt', ArrowLeft: '◄ ', ArrowDown: '▼ ', ArrowRight: '► ', ControlRight: 'Ctrl' };
 
-const Rowkeys1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-const Rowkeys2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'];
-const Rowkeys3 = ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'];
-const Rowkeys4 = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲ ', 'Shift'];
-const Rowkeys5 = ['Ctrl', 'Win', 'Alt', '', 'Alt', '◄ ', '▼ ', '► ', 'Ctrl'];
+const rows = [RowKeys1, RowKeys2, RowKeys3, RowKeys4, RowKeys5];
 
 
 
@@ -17,7 +17,8 @@ const body = document.querySelector('.body');
 
 
 
-//layout the page
+
+// layout the page
 const container = document.createElement('div');
 container.classList.add('container');
 body.append(container);
@@ -62,48 +63,96 @@ info.append(infoPartTwo);
 headerTitle.innerText = 'RSS Виртуальная клавиатура';
 infoPartOne.innerText = 'Клавиатура создана в операционной системе Windows';
 infoPartTwo.innerText = 'Для переключения языка комбинация: левыe ctrl + alt';
-//-
+//
 
 
 
 
 // addEventListener('keydown', e => {
-//   // console.log(e);
-//   // console.log(String.fromCharCode(e.code));
+//   // console.log(key);
 
-//   keysArr.push(e.key);
-//   console.log(keysArr);
+//   // console.log(e);
+
+//   // keysArr.push(e.code);
+//   // console.log(keysArr);
+
 // });
 
 
-function keyCreate(Rowkeys) {
+
+
+function keyCreate(RowKeys) {
   const row = document.createElement('div');
-  for (let keySim of Rowkeys) {
+  for (let keySim in RowKeys) {
     row.classList.add('keyboard-row');
     const key = document.createElement('div');
 
     row.append(key);
 
-    if (keySim.length < 1) {
+    if (RowKeys[keySim].length < 1) {
       key.classList.add('keyboard-row__space', 'key');
-    } else if (keySim.length >= 1 && keySim.length < 5) {
+    } else if (RowKeys[keySim].length >= 1 && RowKeys[keySim].length < 5) {
       key.classList.add('keyboard-row__square', 'key');
     } else {
       key.classList.add('keyboard-row__rectangle', 'key');
     }
 
-    if (keySim.length > 1) {
+    if (RowKeys[keySim].length > 1) {
       key.classList.add('special-key');
     }
 
-    key.innerHTML = keySim;
+    key.classList.add(`${keySim}`);
+    key.innerHTML = RowKeys[keySim];
   }
 
   keyboardKeys.append(row);
 }
 
-keyCreate(Rowkeys1);
-keyCreate(Rowkeys2);
-keyCreate(Rowkeys3);
-keyCreate(Rowkeys4);
-keyCreate(Rowkeys5);
+
+//Call keyCreate for every row
+rows.forEach(item => {
+  keyCreate(item);
+});
+//
+
+
+
+let keyDown = document.querySelectorAll('.key');
+
+//Event key
+addEventListener('keydown', event => {
+  keyDown.forEach(item => {
+    if (item.classList.contains(`${event.code}`)) {
+      item.classList.add('active');
+    } else {}
+  });
+});
+
+addEventListener('keyup', event => {
+  keyDown.forEach(item => {
+    if (item.classList.contains(`${event.code}`)) {
+      item.classList.remove('active');
+    } else {}
+  });
+});
+//
+
+
+//Event mouse
+addEventListener('mousedown', event => {
+  keyDown.forEach(item => {
+    if (item == event.target) {
+      item.classList.add('active');
+    } else {}
+  });
+});
+
+addEventListener('mouseup', event => {
+  keyDown.forEach(item => {
+    if (item == event.target) {
+      item.classList.remove('active');
+    } else {}
+  });
+});
+//
+
